11d3fc96cd2fdff5f034233ff41ba8d5b8c6fdd3
Leo Mancini <mancini@leo.gd>
2018-11-13 09:58:01 -0800

    Getting ready to deploy to Heroku

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 	<head>
-		<title>__TITLE__</title>
+		<title>Portable Profile v2 ("WeChat-style")</title>
 		<link rel="stylesheet/less" href="resources/css/style.less">
 		<script src="resources/js/lib/less.js"></script>
 		<script src="resources/js/lib/jquery.js"></script>
@@ -10,8 +10,29 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
-
 		
-
+		<h3>User Views</h3>
+		<br>
+		<span style="font-size: 32px; vertical-align: -7px;">📱</span>&nbsp;&nbsp;&nbsp;<a href="views/user/new.php" target="_blank">Create Your Profile</a>
+		<br>
+		<br>
+		<span style="font-size: 32px; vertical-align: -7px;">📡</span>&nbsp;&nbsp;&nbsp;<a href="views/user/fill-form.php" target="_blank">Share Your Information with a Provider</a>
+		<br>
+		<br>	
+		<br>
+		<br>
+		<h3>Provider Views</h3>
+		<br>		
+		<span style="font-size: 32px; vertical-align: -7px;">✨</span>&nbsp;&nbsp;&nbsp;<a href="views/provider/form.php" target="_blank">Intake Form with QR Code</a>
+		<br>
+		<br>
+		<span style="font-size: 32px; vertical-align: -7px;">📑</span>&nbsp;&nbsp;&nbsp;<a href="views/provider/generic-form.php" target="_blank">Generic Intake Form</a>
+		<br>
+		<br>	
+		<br>
+		<br>
+		<h3>Debug</h3>
+		<br>	
+		<span style="font-size: 32px; vertical-align: -7px;">⚙️</span>&nbsp;&nbsp;&nbsp;<a href="portable-profile-wechat-style-chrome-ext.zip" target="_blank">Download Chrome Extension</a>
 	</body>
 </html>
--- resources/php/base.php
+++ resources/php/base.php
@@ -1,6 +1,21 @@
 <?php
-	$db_credentials["server"] = "localhost";
-	$db_credentials["username"] = "root";
-	$db_credentials["password"] = "root";
-	$db_credentials["database"] = "qr-code-wechat";
+	if(	$_SERVER["REMOTE_ADDR"] == "localhost" ||
+		$_SERVER["REMOTE_ADDR"] == "leo.local" ||
+		$_SERVER["REMOTE_ADDR"] == "::1" ||
+		strpos($_SERVER["REMOTE_ADDR"], '192.168.1') !== false
+	) {
+		$db_credentials["server"] = "localhost";
+		$db_credentials["username"] = "root";
+		$db_credentials["password"] = "root";
+		$db_credentials["database"] = "qr-code-wechat";
+	} else {
+		$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+		$db_credentials["server"] = $url["host"];
+		$db_credentials["username"] = $url["user"];
+		$db_credentials["password"] = $url["pass"];
+		$db_credentials["database"] = substr($url["path"], 1);
+	}
+	
+	mysqli_set_charset($db, "utf8");		
 ?>
