91bc76bdc5b0436be2021faf801c08219d603fea
Leo Mancini <mancini@leo.gd>
2018-11-20 15:33:55 -0800

    File upload with AWS testing

--- views/user/new.php
+++ views/user/new.php
@@ -30,6 +30,23 @@
 						localStorage.setItem(field.name, field.value);
 					});
 					
+					var file_data = $('#file1').prop('files')[0];   
+				    var form_data = new FormData();                  
+				    form_data.append('file', file_data);
+				    alert(form_data);                             
+				    $.ajax({
+				        url: '../../controllers/upload-file.php', // point to server-side PHP script 
+				        dataType: 'text',  // what to expect back from the PHP script, if anything
+				        cache: false,
+				        contentType: false,
+				        processData: false,
+				        data: form_data,                         
+				        type: 'post',
+				        success: function(php_script_response){
+				            alert(php_script_response); // display response from the PHP script, if any
+				        }
+				     });
+					
 					$("input[type=submit]").val("Saved!").css("background", "#72CE0E");
 					
 					setTimeout(function() {
