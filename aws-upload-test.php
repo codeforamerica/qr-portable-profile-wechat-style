73b035e7d2f9950fbbc9e4f18fcf646c09e6efd0
Leo Mancini <mancini@leo.gd>
2018-11-20 15:11:02 -0800

    File upload with AWS testing

--- aws-upload-test.php
+++ aws-upload-test.php
@@ -5,7 +5,7 @@ $s3 = Aws\S3\S3Client::factory(array(
 	'region' => getenv('BUCKETEER_AWS_REGION'),
 	'version' => 'latest',
     'credentials' => array(
-        'key'    => getenv('BUCKETEER_BUCKET_NAME'),
+        'key'    => getenv('BUCKETEER_AWS_ACCESS_KEY_ID'),
         'secret' => getenv('BUCKETEER_AWS_SECRET_ACCESS_KEY'),
     )
 ));
