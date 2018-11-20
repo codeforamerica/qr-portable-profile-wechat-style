c23ef51a3d485c9268bfcdfd802d62c91527ccd9
Leo Mancini <mancini@leo.gd>
2018-11-20 15:37:31 -0800

    File upload with AWS testing

--- controllers/upload-file.php
+++ controllers/upload-file.php
@@ -13,12 +13,12 @@
 	$bucket = getenv('BUCKETEER_BUCKET_NAME')?: die('No "S3_BUCKET" config var in found in env!');
 	
 	if(	$_SERVER['REQUEST_METHOD'] == 'POST' &&
-		isset($_FILES['userfile']) &&
-		$_FILES['userfile']['error'] == UPLOAD_ERR_OK &&
-		is_uploaded_file($_FILES['userfile']['tmp_name'])
+		isset($_FILES['file1']) &&
+		$_FILES['file1']['error'] == UPLOAD_ERR_OK &&
+		is_uploaded_file($_FILES['file1']['tmp_name'])
 	) {
 	    try {
-	        $upload = $s3->upload($bucket, microtime().$_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+	        $upload = $s3->upload($bucket, microtime().$_FILES['file1']['name'], fopen($_FILES['file1']['tmp_name'], 'rb'), 'public-read');
 			echo htmlspecialchars($upload->get('ObjectURL'));
 		} catch(Exception $error) {
 			echo $error;
